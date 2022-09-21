0bbbea9d573dd8cacc33bf3ad6fff2889a081de2
emirs <emirs@DESKTOP-JFKQICR.local>
2022-09-21 20:42:54 +0300

    First Commit

---
+++ src/firstlesson/javaHelloWorld.java
@@ -0,0 +1,11 @@
+package firstlesson;
+
+public class javaHelloWorld {
+
+	public static void main(String[] args) {
+		// Yorum satırı demektir,bu satır içindekilerin hiçbir kodsal karşılığı yoktur.
+		System.out.println("Hello World"); // "Hello World" yazdırır.
+
+	}
+
+}
